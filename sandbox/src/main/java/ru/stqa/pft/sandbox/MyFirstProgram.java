7ac4e454984f657c20639c8cd0d5d0e42ac2e5af
ArtemBulgakov <art.bulgakov1209@yandex.ru>
2021-05-12 05:48:29 +0500

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,6 +3,26 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello, word guys");
+        hello("word");
+        hello("user");
+        hello("Artem");
+
+        double l = 5;
+        System.out.println("пл. квад со стор. " + l + " = " + area(l));
+        double a = 4;
+        double b = 6;
+        System.out.println("пл. прямоуг. со стор. " + a + " и " + b + " = " + area(a,b));
     }
+    public static void hello (String somebody) {
+        System.out.println("Hello, " + somebody );
+    }
+
+    public static double area (double len) {
+        return len * len;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
+    }
+
 }
